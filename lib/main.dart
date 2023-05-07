4993c85cee48515ba8a4a3381a453655e743a5d7
Ho Duc Luu <hoducluu2022@gmail.com>
2023-05-07 21:02:59 +0700

    cai dat chung thuc voi Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'ui/screens.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthManager(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => ProductsManager(),
         ),
@@ -24,39 +29,50 @@ class MyApp extends StatelessWidget {
           create: (ctx) => OrdersManager(),
         ),
       ],
-      child: MaterialApp(
-        title: 'MyShop',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.purple,
-          ).copyWith(
-            secondary: Colors.deepOrange,
+      child: Consumer<AuthManager>(builder: (context, AuthManager, child) {
+        return MaterialApp(
+          title: 'MyShop',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            fontFamily: 'Lato',
+            colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: Colors.purple,
+            ).copyWith(
+              secondary: Colors.deepOrange,
+            ),
           ),
-        ),
-        home: const ProductsOverviewScreen(),
-        routes: {
-          CartScreen.routeName: (ctx) => const CartScreen(),
-          OrderScreen.routerName: (ctx) => const OrderScreen(),
-          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
-        },
-        onGenerateRoute: ((settings) {
-          if (settings.name == ProductsDetailScreen.routeName) {
-            final productId = settings.arguments as String;
-            return MaterialPageRoute(
-              builder: (ctx) {
-                return EditProductScreen(
-                  productId != null
-                      ? ctx.read<ProductsManager>().findById(productId)
-                      : null,
-                );
-              },
-            );
-          }
-          return null;
-        }),
-      ),
+          home: AuthManager.isAuth
+              ? ProductsOverviewScreen()
+              : FutureBuilder(
+                  future: AuthManager.tryAutoLogin(),
+                  builder: (context, snapshot) {
+                    return snapshot.connectionState == ConnectionState.waiting
+                        ? const SplashScreen()
+                        : const AuthScreen();
+                  },
+                ),
+          routes: {
+            CartScreen.routeName: (ctx) => const CartScreen(),
+            OrderScreen.routerName: (ctx) => const OrderScreen(),
+            UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+          },
+          onGenerateRoute: ((settings) {
+            if (settings.name == ProductsDetailScreen.routeName) {
+              final productId = settings.arguments as String;
+              return MaterialPageRoute(
+                builder: (ctx) {
+                  return EditProductScreen(
+                    productId != null
+                        ? ctx.read<ProductsManager>().findById(productId)
+                        : null,
+                  );
+                },
+              );
+            }
+            return null;
+          }),
+        );
+      }),
       //
       // home: const SafeArea(
       //   child: OrderScreen(),
--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -2,10 +2,15 @@ export 'products/product_overview_sreen.dart';
 export 'products/products_detail_screen.dart';
 export 'products/user_products_screen.dart';
 export 'products/products_manager.dart';
+export 'products/edit_product_screen.dart';
 
 export 'orders/order_manager.dart';
 export 'orders/order_screen.dart';
 
 export 'cart/cart_manager.dart';
 export 'cart/cart_screen.dart';
-export 'products/edit_product_screen.dart';
+
+export 'auth/auth_screen.dart';
+export 'auth/auth_manager.dart';
+
+export 'splash_screen.dart';
